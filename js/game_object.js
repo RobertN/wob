d822b7ac1bc88587cbe7eda1dc5fdd1fb91847f5
Robert Norlander <robert.norlander@gmail.com>
2012-08-18 16:44:43 +0200

    Add getPosition and getVelocity in GameObject

--- js/game_object.js
+++ js/game_object.js
@@ -1,8 +1,20 @@
-exports.GameObject = function()
+exports.GameObject = function(x, y, z)
 {
+	this.pos_x = x;
+	this.pos_y = y;
+	this.pos_z = z;
+
+	this.vel_x = 0;
+	this.vel_y = 0;
+	this.vel_z = 0;
 }
 
 exports.GameObject.prototype.getPosition = function()
 {
-	return {x: 20, y: 20};
+	return {x: this.pos_x, y: this.pos_y, z: this.pos_z};
+}
+
+exports.GameObject.prototype.getVelocity = function() 
+{
+	return {x: vel_x, y: vel_y, z: vel_z};
 }
