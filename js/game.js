4dd913a112693c3812ca5e59161bb252ace8aa30
Jesper Gustin <sirjeppe@gmail.com>
2012-08-19 12:31:58 +0200

    Sunday commit... Fixing up everything, making the block move when pressing arrow keys

--- js/game.js
+++ js/game.js
@@ -1,29 +1,29 @@
-var camera,
-	scene,
+var	scene,
 	renderer,
-	box,
-	sphere,
-	material,
-	boxMesh,
-	sphereMesh;
+	levelBaseURL = 'loadLevel.php';
 
 function init() {
 
-	camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 10000);
+	camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 1000);
 	camera.position.z = 1000;
 
 	scene = new THREE.Scene();
 
-	box = new THREE.CubeGeometry(200, 200, 200);
-	sphere = new THREE.SphereGeometry(60, 20, 20);
-	material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
+	var material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
 
-	boxMesh = new THREE.Mesh(box, material);
-	boxMesh.position.x = 150;
-	boxMesh.position.y = -400;
-	sphereMesh = new THREE.Mesh(sphere, material);
-	scene.add(boxMesh);
-	scene.add(sphereMesh);
+	//
+	// Create a ball
+	//
+	var sphere = new THREE.SphereGeometry(60, 20, 20);
+	var sphereMesh = new THREE.Mesh(sphere, material);
+	createGameObject({ 'id': 'ball', 'isBall': true }, sphereMesh);
+
+	//
+	// Create a mesh
+	//
+	var cube = new THREE.CubeGeometry(200, 200, 200);
+	var cubeMesh = new THREE.Mesh(cube, material);
+	createGameObject({ 'id': 'box' }, cubeMesh);
 
 	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -34,16 +34,97 @@ function init() {
 
 function animate() {
 
-	// note: three.js includes requestAnimationFrame shim
 	requestAnimationFrame(animate);
-
-	boxMesh.position.x -= 1.11;
-
 	renderer.render(scene, camera);
 
+}
+
+function loadLevel(levelID) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			// Fixa
+			var level = JSON.parse(xhr.responseText);
+			for (var i = 0; i < level.gameObjects.length; i++) {
+				createGameObject(level.gameObjects[i]);
+			}
+		}
+	}
+	xhr.open('GET', levelBaseURL + '?levelID=' + levelID, true);
+	xhr.send();
+}
+
+function createGameObject(o, meshObject) {
+
+	//
+	// Setup default object
+	//
+	var gameObject = {
+		'id': 'defaultObject',
+		'isBall': false,
+		'mesh' :meshObject,
+		'collision': true,
+		'collisionCallback': null,
+		'movable': false,
+		'willCollideAt': false
+	}
+
+	//
+	// Handle settings for the object
+	//
+	if (typeof o == 'object') {
+		for (var p in o) {
+			gameObject[p] = o[p];
+		}
+	}
+
+	//
+	// Add game object to the list of game objects
+	//
+	if (typeof gameObjects == 'undefined') {
+		gameObjects = [];
+	}
+	gameObjects[gameObjects.length] = gameObject;
+
+	//
+	// Add mesh object to scene
+	//
+	scene.add(gameObjects[gameObjects.length-1].mesh);
+
+}
+
+function getGameObjectById(id) {
+	for (var i = 0; i < gameObjects.length; i++) {
+		if (gameObjects[i].id == id) {
+			return gameObjects[i];
+		}
+	}
 }
 
 window.onload = function() {
 	init();
 	animate();
 }
+
+document.addEventListener('keydown', function(e) {
+	var cube = getGameObjectById('box');
+	switch (e.keyCode) {
+		// Up
+		case 38:
+			cube.mesh.position.y += 10;
+			break;
+		// Down
+		case 40:
+			cube.mesh.position.y -= 10;
+			break;
+		// Left
+		case 37:
+			cube.mesh.position.x -= 10;
+			break;
+		// Right
+		case 39:
+			cube.mesh.position.x += 10;
+			break;
+			
+	}
+}, false);
---
+++ js/libs/collision.js
@@ -0,0 +1,11 @@
+function detectCollisions() {
+
+	for (var i = 0; i < gameObjects.length; i++) {
+
+		//
+		// Do collision detection
+		//
+		
+	}
+
+}
